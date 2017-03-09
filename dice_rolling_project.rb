02ff3d7ff162736baba1e340db19184cade1be9a
pinknoodlehair <wvunjak1@hotmail.com>
2017-03-09 15:49:05 -0500

    Dice Roll Program
    Made with Ruby.

--- dice_rolling_project.rb
+++ dice_rolling_project.rb
@@ -4,86 +4,54 @@
 
       print "\n""Hello! Welcome to the dice rolling program!"
 
-
-
     #   Asks for the number of dice the user wishes to roll
-
          puts "\n""\n" "Please select the number of dice you wish to roll." 
 
-
-
           #   Gets user input and turns it into 
-
                die_number = gets.chomp 
 
 
-
     ##   Checks to make sure the number is positive
-
-         while !/\A\d+\z/.match(die_number) == true do
-
+          while !/\A\d+\z/.match(die_number) == true do
 
 
         #    Asks user to re-enter number for number of die to be rolled if input is not an integer
-
               puts "\n""\n" "I'm sorry, I didn't quite catch that. How many dice do you wish to roll?"
-
                die_number = gets.chomp
-
                 end
 
 
                   #    Assigns user input to a new variable 
-
                         die_number_success = die_number
-
-                        die_number_success = die_number_success.to_i
-
+                         die_number_success = die_number_success.to_i
 
 
   ##    Tells the user the number of die chosen and asks how many sides they wish for each die to have
-
-        puts "\n""\n" "So you want to roll #{die_number_success} die? How many sides are there?"
-
-         sides_number = gets.chomp
-
-
+         puts "\n""\n" "So you want to roll #{die_number_success} die? How many sides are there?"
+          sides_number = gets.chomp
 
         #   Checks to make sure the number is positive
-
-            while !/\A\d+\z/.match(sides_number) == true do
-
-
+             while !/\A\d+\z/.match(sides_number) == true do
 
           #    Asks user to re-enter number for number of die to be rolled if input is not an integer
-
-               puts "\n""\n" "Excuse me, I don't understand. How many sides should there be to each die?"
-
-                sides_number = gets.chomp
-
-                 end
-
+                puts "\n""\n" "Excuse me, I don't understand. How many sides should there be to each die?"
+                 sides_number = gets.chomp
+                  end
 
                         #    Assigns user input to a new variable 
-
                              sides_number_success = sides_number
-
-                             sides_number_success = sides_number_success.to_i
-
-
+                              sides_number_success = sides_number_success.to_i
 
 
   ##  Plugs in values from die_number_success and sides_number_success to create desired output
 
-
       #   Creates formula to roll dice.
-
          die_number_success.times do |die_number_success|
 
-          puts rand(1..sides_number_success)
-          puts "\n"
-
-            end
+          #    Puts each of the final answers on a new line telling the user what they rolled.
+                 puts "\n" "Rolled:"
+                  puts rand(1..sides_number_success)
+                    end
 
 
    
